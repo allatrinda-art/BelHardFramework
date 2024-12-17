c3cea98a745817a368cf6a87ac1b193a335811cc
allatrinda-art <alla.trinda@gmail.com>
2024-12-18 01:03:41 +0300

    Added the 1st homework

--- JavaFramework/src/Main.java
+++ JavaFramework/src/Main.java
@@ -1,15 +1,74 @@
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-        // to see how IntelliJ IDEA suggests fixing it.
-        System.out.println("Hello and welcome!");
+        //Task 1
+        String name = "Alla";
+        String surname = "Trinda";
+        String profession = "QA Engineer";
+        String myInfo = String.format("My name and surname are %s %s. My profession is %s", name, surname, profession);
+        System.out.println(myInfo);
 
-        for (int i = 1; i <= 5; i++) {
-            //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-            // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-            System.out.println("i = " + i);
-        }
+        //Task 2
+        boolean bool = true;
+        char ch = 'A';
+        byte b = -128;
+        short s = 32;
+        int i = 10;
+        long l = 3200L;
+        float f = 12.34f;
+        double d = 11.99;
+        String string = "My new string";
+        String allVar = String.format("Here is all variables with their values that I remember: boolean - %s, char - %c, byte - %d, short - %d, int - %d, long - %d, %float - f, double - %f, String - %s.", bool, ch, b, s, i, l, f, d, string);
+        System.out.println(allVar);
+
+        /*StringBuffer*/
+        StringBuffer changableName = new StringBuffer(name);
+        changableName.append("Maria");
+        changableName.append("Prekrasnaya");
+        System.out.println("Now my name is: " + changableName);
+
+        //!!!Task 3!!!
+        System.out.print("Please enter your greeting: ");
+        Scanner sc = new Scanner(System.in);
+        String greeting = sc.nextLine();
+        System.out.println("Welcome aboard: " + greeting);
+
+        /*lenth method*/
+        System.out.println(greeting.length());
+
+        /*charAt method*/
+        System.out.println(greeting.charAt(3));
+
+        /*isEmpty method*/
+        System.out.println(greeting.isEmpty()); //is always false because of Scanner method above
+
+        /*equals method*/
+        System.out.print("Type here your presentation: ");
+        String presentation = sc.nextLine();
+        System.out.println("Your presentaion is: " + presentation);
+        boolean wordsAreEqual = greeting.equals(presentation);
+        System.out.println("So, your greeting and presentation are equal? - " + wordsAreEqual);
+
+        /*equalsIgnoreCase method*/
+        boolean wordsAreEqualCaseSensitive = greeting.equalsIgnoreCase(presentation);
+        System.out.println("Are they equal case sensitive? - " + wordsAreEqualCaseSensitive);
+
+        /*startWith, endsWith, contains methods*/
+        boolean startsWithHello = greeting.startsWith("Hello");
+        boolean endsWithAnna = greeting.endsWith("Anna");
+        boolean containsName = greeting.contains("name");
+        String containMethods = String.format("Did you say Hello? - %s. Your name is Anna? - %s. Did you mention your name? - %s", startsWithHello, endsWithAnna, containsName);
+        System.out.println(containMethods);
+
+        /*replace method*/
+        String updatedGreeting = greeting.replace('H', 'h');
+        String updatedName = greeting.replace("Anna", "Zoya");
+        String newGreeting = String.format("My new greeting is: %s. And with my real name: %s", updatedGreeting, updatedName);
+        System.out.println(newGreeting);
+
+        /*ToLowerCase method*/
+        String lowerCaseGreeting = updatedGreeting.toLowerCase();
+        System.out.println("The greeting with lowercase: " + lowerCaseGreeting);
     }
 }
